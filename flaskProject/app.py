d85568ea2d14839c76eed9927a5ef305862dae83
ZaninaMehdi <77996425+ZaninaMehdi@users.noreply.github.com>
2024-11-03 11:04:39 -0500

    feat/sentiment route

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -7,6 +7,7 @@ from api.resources.analysis import AnalysisResource
 from api.resources.technical_analysis import TechnicalAnalysisResource
 from api.resources.news import NewsResource
 from api.resources.bedrock import BedrockResource
+import api.resources.sentiment_analysis as Sentiment
 
 app = Flask(__name__)
 CORS(app)  # Initialiser CORS pour l'application
@@ -15,6 +16,7 @@ api = Api(app)
 # Configuration des routes
 api.add_resource(AnalysisResource, '/api/analysis')
 api.add_resource(TechnicalAnalysisResource, '/api/technical_analysis')
+api.add_resource(Sentiment, '/api/sentiment_analysis')
 api.add_resource(NewsResource, '/api/news')
 api.add_resource(BedrockResource, '/api/bedrock')
 
